0d46d5d81de014f30ebdc24ec734e8e6b0c7cd37
dlopez1591 <dlopez1591@gmail.com>
2023-11-11 10:00:03 -0500

    configurado repositorio de administrador

--- src/main/java/com/daniellopez/CRUD/repositories/AdministradorRepository.java
+++ src/main/java/com/daniellopez/CRUD/repositories/AdministradorRepository.java
@@ -1,4 +1,10 @@
 package com.daniellopez.CRUD.repositories;
 
-public interface AdministradorRepository {
+import com.daniellopez.CRUD.models.Administrador;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface AdministradorRepository extends JpaRepository<Administrador, Long> {
+
 }
